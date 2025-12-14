5ee43944cbd7e7ba539d70496eb26b319b959e9f
GuillaumeCldn <gclaudon@yahoo.com>
2025-12-15 00:07:17 +0100

    Added kerbovim plugin & more keybinds

--- lightVim/.config/nvim/init.lua
+++ lightVim/.config/nvim/init.lua
@@ -1,22 +1,69 @@
 -- General --
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.swapfile = false
+vim.opt.wrap = false
+
+-- Indentation --
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.signcolumn = "yes"
-vim.opt.swapfile = false
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Search settings --
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Visual settings --
 vim.opt.winborder = "rounded"
 vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.winblend = 0
+vim.opt.pumblend = 10
+vim.opt.pumheight = 10
+vim.opt.completeopt = "noinsert,noselect"
 
--- Keymaps --
+-- Keymaps-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>")
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
 vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
 
--- TODO: Lualine config --
--- Package installation --
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Copy Full File-Path
+vim.keymap.set("n", "<leader>pa", function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	print("file:", path)
+end)
+
+-- Basic autocommands
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+
+-- TODO: Lualine config
+-- Package installation
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
@@ -26,7 +73,7 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/xiyaowong/transparent.nvim" },
+	{ src = "https://github.com/Freedzone/kerbovim" },
 	{ src = "https://github.com/folke/todo-comments.nvim" },
 })
 
@@ -42,15 +89,17 @@ require("mason").setup({
 	},
 })
 require("mason-lspconfig").setup()
-require("transparent").setup({ extra_groups = { "NormalFloat" } })
 require("todo-comments").setup()
 
 -- TODO: Python code highlighting is still limited --
 -- Language servers --
 vim.lsp.config("lua_ls", { settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } } })
 
--- Colorscheme --
+-- Colorscheme & background --
 vim.cmd.colorscheme("catppuccin-macchiato")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
 -- Status line --
 vim.cmd(":hi statusline guibg=NONE")
