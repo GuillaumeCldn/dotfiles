2b358a941e85b8f9d7c9788c88ad7f98a8aee626
GuillaumeCldn <gclaudon@yahoo.com>
2025-12-18 13:49:30 +0100

    Added Lualine & Gitsigns plugins, cleaned Kanagawa colorscheme from config

--- lightNvim/.config/nvim/init.lua
+++ lightNvim/.config/nvim/init.lua
@@ -90,6 +90,8 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/Freedzone/kerbovim" },
 	{ src = "https://github.com/folke/todo-comments.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 
 -- Package activation --
@@ -105,18 +107,8 @@ require("mason").setup({
 })
 require("mason-lspconfig").setup()
 require("todo-comments").setup()
-require("kanagawa").setup({
-	colors = {
-		theme = {
-			all = {
-				ui = {
-					bg_gutter = "none"
-					}
-				}
-			}
-		}
-	}
-)
+require("lualine").setup()
+require("gitsigns").setup()
 
 -- FIX: Python code highlighting is still limited
 
@@ -137,8 +129,8 @@ vim.cmd(":hi statusline guibg=NONE")
 -- ============================================================================
 
 -- Tab display settings
-vim.opt.showtabline = 1  -- Always show tabline (0=never, 1=when multiple tabs, 2=always)
-vim.opt.tabline = ''     -- Use default tabline (empty string uses built-in)
+vim.opt.showtabline = 1 -- Always show tabline (0=never, 1=when multiple tabs, 2=always)
+vim.opt.tabline = "" -- Use default tabline (empty string uses built-in)
 
 -- Transparent tabline appearance
 vim.cmd([[
@@ -146,66 +138,66 @@ vim.cmd([[
 ]])
 
 -- Alternative navigation (more intuitive)
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', { desc = 'Close tab' })
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { desc = "New tab" })
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close tab" })
 
 -- Tab moving
-vim.keymap.set('n', '<leader>tm', ':tabmove<CR>', { desc = 'Move tab' })
-vim.keymap.set('n', '<leader>t>', ':tabmove +1<CR>', { desc = 'Move tab right' })
-vim.keymap.set('n', '<leader>t<', ':tabmove -1<CR>', { desc = 'Move tab left' })
+vim.keymap.set("n", "<leader>tm", ":tabmove<CR>", { desc = "Move tab" })
+vim.keymap.set("n", "<leader>t>", ":tabmove +1<CR>", { desc = "Move tab right" })
+vim.keymap.set("n", "<leader>t<", ":tabmove -1<CR>", { desc = "Move tab left" })
 
 -- Function to open file in new tab
 local function open_file_in_tab()
-  vim.ui.input({ prompt = 'File to open in new tab: ', completion = 'file' }, function(input)
-    if input and input ~= '' then
-      vim.cmd('tabnew ' .. input)
-    end
-  end)
+	vim.ui.input({ prompt = "File to open in new tab: ", completion = "file" }, function(input)
+		if input and input ~= "" then
+			vim.cmd("tabnew " .. input)
+		end
+	end)
 end
 
 -- Function to duplicate current tab
 local function duplicate_tab()
-  local current_file = vim.fn.expand('%:p')
-  if current_file ~= '' then
-    vim.cmd('tabnew ' .. current_file)
-  else
-    vim.cmd('tabnew')
-  end
+	local current_file = vim.fn.expand("%:p")
+	if current_file ~= "" then
+		vim.cmd("tabnew " .. current_file)
+	else
+		vim.cmd("tabnew")
+	end
 end
 
 -- Function to close tabs to the right
 local function close_tabs_right()
-  local current_tab = vim.fn.tabpagenr()
-  local last_tab = vim.fn.tabpagenr('$')
+	local current_tab = vim.fn.tabpagenr()
+	local last_tab = vim.fn.tabpagenr("$")
 
-  for i = last_tab, current_tab + 1, -1 do
-    vim.cmd(i .. 'tabclose')
-  end
+	for i = last_tab, current_tab + 1, -1 do
+		vim.cmd(i .. "tabclose")
+	end
 end
 
 -- Function to close tabs to the left
 local function close_tabs_left()
-  local current_tab = vim.fn.tabpagenr()
+	local current_tab = vim.fn.tabpagenr()
 
-  for i = current_tab - 1, 1, -1 do
-    vim.cmd('1tabclose')
-  end
+	for i = current_tab - 1, 1, -1 do
+		vim.cmd("1tabclose")
+	end
 end
 
 -- Enhanced keybindings
-vim.keymap.set('n', '<leader>tO', open_file_in_tab, { desc = 'Open file in new tab' })
-vim.keymap.set('n', '<leader>td', duplicate_tab, { desc = 'Duplicate current tab' })
-vim.keymap.set('n', '<leader>tr', close_tabs_right, { desc = 'Close tabs to the right' })
-vim.keymap.set('n', '<leader>tL', close_tabs_left, { desc = 'Close tabs to the left' })
+vim.keymap.set("n", "<leader>tO", open_file_in_tab, { desc = "Open file in new tab" })
+vim.keymap.set("n", "<leader>td", duplicate_tab, { desc = "Duplicate current tab" })
+vim.keymap.set("n", "<leader>tr", close_tabs_right, { desc = "Close tabs to the right" })
+vim.keymap.set("n", "<leader>tL", close_tabs_left, { desc = "Close tabs to the left" })
 
 -- Function to close buffer but keep tab if it's the only buffer in tab
 local function smart_close_buffer()
-  local buffers_in_tab = #vim.fn.tabpagebuflist()
-  if buffers_in_tab > 1 then
-    vim.cmd('bdelete')
-  else
-    -- If it's the only buffer in tab, close the tab
-    vim.cmd('tabclose')
-  end
+	local buffers_in_tab = #vim.fn.tabpagebuflist()
+	if buffers_in_tab > 1 then
+		vim.cmd("bdelete")
+	else
+		-- If it's the only buffer in tab, close the tab
+		vim.cmd("tabclose")
+	end
 end
-vim.keymap.set('n', '<leader>bd', smart_close_buffer, { desc = 'Smart close buffer/tab' })
+vim.keymap.set("n", "<leader>bd", smart_close_buffer, { desc = "Smart close buffer/tab" })
