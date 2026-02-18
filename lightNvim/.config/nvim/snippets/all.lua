6b28cb9509e4ead64c907c3970bc5c412db3a8a9
GuillaumeCldn <gclaudon@yahoo.com>
2026-02-18 17:53:24 +0100

    Small change

--- lightNvim/.config/nvim/snippets/all.lua
+++ lightNvim/.config/nvim/snippets/all.lua
@@ -12,37 +12,4 @@ return {
 			}
 		)
 	),
-	s("snip",
-		fmta([[
-		s(
-			{
-			trig = "<>",
-			snippetType = "<>",
-			condition = "<>",
-			},
-			{
-				<>
-			},
-		)
-		]],
-			{
-				i(1, "trigger"),
-				c(2, {
-					t("autosnippet"),
-					i(nil, "other")
-				}),
-				c(3, {
-					t("in_mathzone"),
-					t("in_equation"),
-					t("in_text"),
-					t("in_comment"),
-					t("in_env"),
-					t("in_tikz"),
-					t("in_itemize"),
-					i(nil, "other"),
-				}),
-				i(0)
-			}
-		)
-	),
 }
