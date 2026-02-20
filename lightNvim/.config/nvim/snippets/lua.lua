0287c15f7e7620c43772325174edfdb769a5c9b1
GuillaumeCldn <gclaudon@yahoo.com>
2026-02-20 19:51:11 +0100

    refined snippet boilerplate snippet

--- lightNvim/.config/nvim/snippets/lua.lua
+++ lightNvim/.config/nvim/snippets/lua.lua
@@ -1,25 +1,24 @@
 require("luasnip-helper-funcs")
 local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
 local fmta = require("luasnip.extras.fmt").fmta
 
-return{
-	s("snip",
+return {
+	s({ trig = "snip", snippetType = "autosnippet" },
 		fmta([[
-		s(
-			{
-			trig = "<>",
-			snippetType = "<>",
-			condition = <>,
-			},
-			{
+			s(
+				{
+				trig = "<>",
+				snippetType = "<>",
+				condition = <>,
+				},
 				<>
-			},
-		)
-		]],
+			),
+			]],
 			{
 				i(1, "trigger"),
 				c(2, {
@@ -28,15 +27,19 @@ return{
 				}),
 				c(3, {
 					t("in_mathzone"),
+					i(nil, "other"),
 					t("in_equation"),
 					t("in_text"),
 					t("in_comment"),
 					t("in_env"),
 					t("in_tikz"),
 					t("in_itemize"),
-					i(nil, "other"),
 				}),
-				i(0)
+				c(4, {
+					sn(nil, { t({"{", "		"}), i(1), t({"","	}"}) }),
+					sn(nil, { t({"fmt([[", "		"}), i(1), t({"","	]],", "	{", "		"}), i(2), t({"","	})"}) }),
+					sn(nil, { t({"fmta([[", "		"}), i(1), t({"","	]],", "	{", "		"}), i(2), t({"","	})"}) }),
+				})
 			}
 		)
 	),
