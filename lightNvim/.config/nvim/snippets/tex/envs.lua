b84345f7359c8d21cc2385d6a54c0fb568bd52ae
GuillaumeCldn <gclaudon@yahoo.com>
2026-02-20 21:02:35 +0100

    Added LaTeX environment snippets file

---
+++ lightNvim/.config/nvim/snippets/tex/envs.lua
@@ -0,0 +1,28 @@
+require("luasnip-helper-funcs")
+local ls = require("luasnip")
+local s = ls.snippet
+
+local rep = require("luasnip.extras").rep
+local fmta = require("luasnip.extras.fmt").fmta
+
+local tex_utils = require("tex.utils")
+local in_text = tex_utils.in_text
+
+return {
+	s(
+		{
+		trig = "en",
+		condition = in_text,
+		},
+		fmta([[
+			\begin{<>}
+				<>
+			\end{<>}
+		]],
+		{
+			i(1),
+			i(0),
+			rep(1)
+		})
+	),
+}
